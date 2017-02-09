a1dbd97ffa9fb9d8ae830bae4c9c72af41d90adf
Unknown <stratosfr@gmail.com>
2017-02-09 13:31:28 +0100

    Optimize CSS output file
    Now CSS for prod are minified and optimized

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -6,6 +6,7 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 var env = config.build.env
 
 var webpackConfig = merge(baseWebpackConfig, {
@@ -34,6 +35,12 @@ var webpackConfig = merge(baseWebpackConfig, {
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css')
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments : {removeAll: true } },
+      canPrint: true
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
