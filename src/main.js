7dc93f1794a991c4d3c8920b217871aab6e0c8d9
Unknown <stratosfr@gmail.com>
2017-02-09 14:06:17 +0100

    First attempt to made basic Auth
    Actually All page if not Auth redirect to / (which is login)
    /logout deAuth the session
    Credentials are localy stored (need to connect to API)

--- src/main.js
+++ src/main.js
@@ -2,18 +2,36 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
+import auth from './utils/auth'
+
 import App from './components/App.vue'
 
-import Login from './components/Auth.vue'
+import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 
 require('font-awesome-loader');
 
+function requireAuth (to, from, next) {
+  if (!auth.loggedIn()) {
+    next({
+      path: '/',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+}
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/home', component: Home },
-    { path: '/', component: Login }
+    { path: '/home', component: Home, beforeEnter: requireAuth },
+    { path: '/', component: Login },
+    { path: '/logout',
+      beforeEnter (to, from, next) {
+        auth.logout()
+        next('/')
+      }}
   ]
 })
 
